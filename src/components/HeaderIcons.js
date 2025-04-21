9805e372112d4e2c4772cdc513c363cb0d47e7c5
adamjohnes <70388982+adamjohnes@users.noreply.github.com>
2025-04-21 11:34:33 -0500

    resume changes

--- src/components/HeaderIcons.js
+++ src/components/HeaderIcons.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import xLogo from '../imgs/x-logo.AVIF'
+// import xLogo from '../imgs/x-logo.AVIF'
 import linkedinLogo from '../imgs/linkedin-logo.AVIF'
 import githubLogo from '../imgs/github-logo.AVIF'
 
@@ -7,12 +7,14 @@ const HeaderIcons = () =>
 {
   return (
     <div className='flex flex-col justify-center items-center'>
+      { /*
       <a className='w-4/12 sm:w-3/12' href='https://twitter.com/AdamJohnes_'>
         <img className='p-1 border border-transparent min-w-50
                             hover:rounded-full
                           hover:border-white'
              alt='x logo' src={ xLogo }></img>
       </a>
+      */ }
       <a className='w-4/12 sm:w-3/12' href='https://www.linkedin.com/in/adam-johnes/'>
         <img className='p-1 border border-transparent min-w-50
                             hover:rounded-full
